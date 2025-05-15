0b125dfe423f7f1b8c5eff252621c0689ab05a2f
Sanju-yatnam <sanju@yatnam.com>
2025-05-15 12:32:41 +0530

    New style changes

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Admin Login - Shopkart</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100 bg-light">
     
